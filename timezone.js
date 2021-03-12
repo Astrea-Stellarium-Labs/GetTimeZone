a4de356e6d85224264c528556adc83f6f5f2a54f
Sonic4999 <sonictbm@gmail.com>
2021-03-12 12:18:12 -0500

    More reliable way of getting time zone

--- timezone.js
+++ timezone.js
@@ -1,15 +1,8 @@
-function get_timezone(dt) 
-{ 
-  // honestly, i'm not a javascript developer, but i can somewhat explain this
-  // so the first part is regex - all it does is get anything thats enclosed in parenthesis
-  // then we execute the regex statement on a javascript date object thats been convert to string
-  // date objects look like this: Thu Mar 11 2021 00:00:00 GMT-0500 (Eastern Standard Time)
-  // and our timezone is conveniently enclosed in parenthesis
-  // so the regex gets that out, and the [1] part is needed otherwise our output
-  // would still have the parenthesis
-  return /\((.*)\)/.exec(dt.toString())[1];
-}
+// ultimately, using a library to parse time zones
+// is much safer than using regex to do so
+// since libraries are not dependent on how the individual
+// device will format the timezone
 
-dt = new Date(); 
-var timezone = get_timezone(dt);
+dt = luxon.DateTime.now();
+var timezone = dt.offsetNameShort
 document.getElementById("timezone_text").innerText = "Your time zone is: " + timezone
