715497f24ad0b8e7dcff2f7d991d884071396c02
Sonic4999 <sonictbm@gmail.com>
2021-12-04 16:25:26 -0500

    Added full name support

--- timezone.js
+++ timezone.js
@@ -3,14 +3,25 @@
 // but it should be fine
 
 let date = Date.now();
-const dtFormat = new Intl.DateTimeFormat([], {"timeZoneName": "short"});
 
-const offsetNameFormat = dtFormat
+const shortDTFormat = new Intl.DateTimeFormat([], { "timeZoneName": "short" });
+const offsetNameFormat = shortDTFormat
     .formatToParts(date)
     .find((m) => m.type.toLowerCase() === "timezonename");
-
 const offsetName = offsetNameFormat.value;
-const zoneName = dtFormat.resolvedOptions().timeZone;
-
 document.getElementById("timezone_text").innerText = "Your time zone is: " + offsetName
-document.getElementById("othername_text").innerText = "Otherwise known as: " + zoneName
+
+const longDTFormat = new Intl.DateTimeFormat([], { "timeZoneName": "long" });
+const longOffsetNameFormat = longDTFormat
+    .formatToParts(date)
+    .find((m) => m.type.toLowerCase() === "timezonename");
+const longOffsetName = longOffsetNameFormat.value;
+document.getElementById("fullname_text").innerText = "Full name: " + longOffsetName
+
+const zoneName = shortDTFormat.resolvedOptions().timeZone;
+var temp_link = document.createElement("a");
+temp_link.href = "https://www.iana.org/time-zones";
+temp_link.target = '_blank';
+temp_link.innerHTML = "IANA";
+
+document.getElementById("othername_text").innerHTML = temp_link.outerHTML + " name: " + zoneName;
