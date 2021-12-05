7674ad336c3fc979e699b612558e1be33e881aa6
Astrea <sonictbm@gmail.com>
2021-12-04 20:24:14 -0500

    Made names a bit shorter (#4)
    * Create minify.yml
    
    * Shorted much of the logic for timezone getting
    
    * Made names a bit shorter

--- timezone.js
+++ timezone.js
@@ -1,1 +1,21 @@
-let date=Date.now();function getTimeZoneName(e){return new Intl.DateTimeFormat([],{timeZoneName:e}).formatToParts(date).find((e=>"timeZoneName"===e.type)).value}document.getElementById("timezone_text").innerText="Your time zone is: "+getTimeZoneName("short"),document.getElementById("fullname_text").innerText="Full name: "+getTimeZoneName("long");const zoneName=(new Intl.DateTimeFormat).resolvedOptions().timeZone;var temp_link=document.createElement("a");temp_link.href="https://www.iana.org/time-zones",temp_link.innerHTML="IANA",document.getElementById("othername_text").innerHTML=temp_link.outerHTML+" name: "+zoneName;
+// note that i'm not really a javascript programmer
+// so what's here might not be amazing
+// but it should be fine
+
+function getTZName(type) {
+    const DTFormat = new Intl.DateTimeFormat([], { "timeZoneName": type });
+    const tzName = DTFormat
+        .formatToParts(Date.now())
+        .find((m) => m.type === "timeZoneName");
+    return tzName.value;
+}
+
+document.getElementById("timezone").innerText = "Your time zone is: " + getTZName("short")
+document.getElementById("fullname").innerText = "Full name: " + getTZName("long")
+
+const ianaName = new Intl.DateTimeFormat().resolvedOptions().timeZone;
+var temp_link = document.createElement("a");
+temp_link.href = "https://www.iana.org/time-zones";
+temp_link.innerHTML = "IANA";
+
+document.getElementById("iananame").innerHTML = temp_link.outerHTML + " name: " + ianaName;
