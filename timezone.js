81e77846ef61b17a614d911315156e60b62bd7f2
Astrea49 <Astrea49@users.noreply.github.com>
2022-09-08 02:33:31 +0000

    Minify source code

--- timezone.js
+++ timezone.js
@@ -1,17 +1,1 @@
-// note that i'm not really a javascript programmer
-// so what's here might not be amazing
-// but it should be fine
-
-function getTZName(type) {
-    const DTFormat = new Intl.DateTimeFormat([], { "timeZoneName": type });
-    const tzName = DTFormat
-        .formatToParts(Date.now())
-        .find((m) => m.type === "timeZoneName");
-    return tzName.value;
-}
-
-document.getElementById("timezone").innerText = "Your time zone is: " + getTZName("short")
-document.getElementById("fullname").innerText = "Full name: " + getTZName("long")
-
-const ianaName = new Intl.DateTimeFormat().resolvedOptions().timeZone;
-document.getElementById("iananame").innerHTML = "IANA name: " + ianaName;
+function getTZName(e){return new Intl.DateTimeFormat([],{timeZoneName:e}).formatToParts(Date.now()).find((e=>"timeZoneName"===e.type)).value}document.getElementById("timezone").innerText="Your time zone is: "+getTZName("short"),document.getElementById("fullname").innerText="Full name: "+getTZName("long");const ianaName=(new Intl.DateTimeFormat).resolvedOptions().timeZone;document.getElementById("iananame").innerHTML="IANA name: "+ianaName;
